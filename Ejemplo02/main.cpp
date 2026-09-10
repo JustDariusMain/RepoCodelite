cb8b72c7c29c1f82a7b3346273682a593012ae30
Darius <dariusdomingo16@gmail.com>
2026-09-10 16:57:46 +0200

    Subida de Ejercicios
    Subo los ejercicios creados para estructura de datos, de forma obligatoria para la asignatura

---
+++ Ejemplo02/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+    int x,y,z;
+    cout << "introduce el primer número: ";
+    cin >> x;
+    cout << "Introduce el segundo número: ";
+    cin >> y;
+    z = x + y;
+    cout << "La suma " <<x<< " más " <<y<< " es igual a " << z << "." << endl;
+}
